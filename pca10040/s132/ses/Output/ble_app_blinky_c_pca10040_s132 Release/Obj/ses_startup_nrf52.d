2e96998812834d40a4c8183e307bc2192e54b18f
Irina <irina15anohina@gmail.com>
2020-05-28 15:04:58 +0300

    Read ADC data, led on when data > 15 (1.5V)

---
+++ pca10040/s132/ses/Output/ble_app_blinky_c_pca10040_s132 Release/Obj/ses_startup_nrf52.d
@@ -0,0 +1,2 @@
+Output/ble_app_blinky_c_pca10040_s132\ Release/Obj/ses_startup_nrf52.o: \
+ C:\Ira\Ira\Ira\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\ses_startup_nrf52.s
---
+++ pca10040/s132/ses/Output/ble_app_blinky_c_pca10040_s132 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_blinky_c_pca10040_s132\ Release/Obj/utf.o: \
+ C:\Ira\Ira\Ira\nRF5_SDK_16.0.0_98a08e2\external\utf_converter\utf.c \
+ C:\Ira\Ira\Ira\nRF5_SDK_16.0.0_98a08e2\external\utf_converter\utf.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52a/include/stddef.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52a/include/__crossworks.h \
+ C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52a/include/stdint.h
